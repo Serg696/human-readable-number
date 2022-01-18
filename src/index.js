c967f1fe453be8d009a8da4eb8c4cc05366d0dbb
Serg696 <88836338+Serg696@users.noreply.github.com>
2022-01-18 13:31:34 +0300

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
+	let numbers = [0,1,2,3,4,5,6,7,8,9];
+  	let strings = ["zero","one","two","three","four","five", "six", "seven", "eight", "nine"];
+	//function toRedable(n){
+    	let num = String(number).split('');
+    	let res = '';
+    	console.log(num);
+    	num.forEach((elem)=>{
+     		numbers.forEach((item, index)=>{
+       	 		if (Number(elem) === item){
+          			// console.log(typeof elem + " " + el + " " +strings[ind]);
+          			// console.log(elem + " " + index);
+          			res += strings[index] + " "
+        		}
+      		});
+    	})
+    	if (num.length > 2){
+      		res = res.split(" ");
+      		res[0] = res[0] + " hundred";
+		if (res[1] === "zero"){
+        		res = res.filter((item)=> item !== "zero");
+      		}
+      		res = res.join(" ")
+    	}
+   	 //console.log(res);
+	return res;
+};
   
-}
+
